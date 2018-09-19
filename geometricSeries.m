c52a31ae9ef857391ceeb49b07c850da7b14fc1d
Sam Cole <scole3@uic.edu>
2018-09-19 15:49:14 -0500

    Make geometricSeries function global

---
+++ geometricSeries.m
@@ -0,0 +1,10 @@
+% Compute the sum of the first N terms of the  geometric series P^0 + P^1 + P^2 + ...
+% P may be scalar or matrix valued
+function W = geometricSeries(P, N)
+  n = size(P, 1);
+  if N == 0
+    W = eye(n);
+  else
+    W = eye(n) + P * geometricSeries(P, N - 1);
+  end
+end
--- naiveIterativeKemeny.m
+++ naiveIterativeKemeny.m
@@ -1,14 +1,3 @@
 function K = naiveIterativeKemeny(P, N)
   K = trace(geometricSeries(P, N)) - N - 1;
 end
-
-% Compute the sum of the first N terms of the  geometric series P^0 + P^1 + P^2 + ...
-% P may be scalar or matrix valued
-function W = geometricSeries(P, N)
-  n = size(P, 1);
-  if N == 0
-    W = eye(n);
-  else
-    W = eye(n) + P * geometricSeries(P, N - 1);
-  end
-end
