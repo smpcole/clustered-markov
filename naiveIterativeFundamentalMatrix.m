d41a8a06e546b5b6d3b924d43ecbb346d02ab9fc
Sam Cole <scole3@uic.edu>
2018-09-20 11:05:05 -0500

    Approximate A with a large power of P in naiveIterativeFundamentalMatrix

--- naiveIterativeFundamentalMatrix.m
+++ naiveIterativeFundamentalMatrix.m
@@ -1,8 +1,7 @@
 % Compute the fundamental matrix of a regular Markov chain iteratively
 function Z = naiveIterativeFundamentalMatrix(P, N)
-  pi = iterativeStationaryDistr(P, N);
   n = size(P, 1);
-  A = ones(n, 1) * pi';
+  A = P^(2 * N); % Approximate A with a large power of P
   % Z = geometricSeries(P - A, N);
   Z = geometricSeries(P, N) - N * A; % Which of these is better?
 end
