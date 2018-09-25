de54cd28f8152b914dac8ef05bfbca4b64ed0be1
Sam Cole <scole3@uic.edu>
2018-09-24 19:11:46 -0500

    Add Python script to generate graph from CSV using Matplotlib

---
+++ genoutput.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import matplotlib.pyplot as plt
+import sys
+import numpy as np
+
+csvpath = sys.argv[1]
+outpath = sys.argv[2]
+
+Y = np.genfromtxt(csvpath, delimiter = ',')
+
+# If Y is a vector, cast it as a 1 x n matrix
+if Y.ndim == 1:
+    Y.shape = (1, Y.shape[0])
+
+x = np.arange(1, Y.shape[1] + 1)
+
+for i in range(Y.shape[0]):
+    plt.plot(x, Y[i])
+
+plt.savefig(outpath)
+print("Output written to %s" % outpath)
