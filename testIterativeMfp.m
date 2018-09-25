24d1c3e6a4aca19c07491e97d170000dbd7410eb
Sam Cole <scole3@uic.edu>
2018-09-24 22:23:54 -0500

    Move randomIndices helper function to its own file

--- testIterativeMfp.m
+++ testIterativeMfp.m
@@ -11,38 +11,3 @@ function Y = testIterativeMfp(sizes, intra, inter, N, outpath)
   fprintf('Output written to %s\n', outpath);
   
 end
-
-% Choose two random pairs of indices, one from the same cluster and one from different clusters
-function indices = randomIndices(sizes)
-  n = sum(sizes);
-  k = length(sizes);
-
-  % Choose a random cluster
-  i = randi(k);
-  s = sizes(i);
-
-  % Choose a random state in that cluster
-  u1 = sum(sizes(1:i - 1)) + randi(s);
-
-  % Choose a different random state in that cluster
-  v1 = sum(sizes(1:i - 1)) + randi(s - 1);
-  if v1 >= u1
-    v1 = v1 + 1;
-  end
-
-  % Choose another random cluster (may be same as first)
-  i = randi(k);
-  s = sizes(i);
-
-  % Choose a random state in that cluster
-  u2 = sum(sizes(1:i - 1)) + randi(s);
-
-  % Choose a random state in a different cluster
-  v2 = randi(n - s);
-  if v2 > sum(sizes(1:i - 1))
-    v2 = v2 + s;
-  end
-
-  indices = [u1, v1; u2, v2];
-
-end
