07b37a2185959a36e3aed5ac116512c1b9f0fa5c
Sam Cole <scole3@uic.edu>
2018-11-29 09:38:57 -0600

    Add 'type' parameter to pointedBlockChain function
    If type == 1, each clique has a single "point" connected to all the
    other points; thus, the points form a k-clique.
    
    If type == 2, each clique has k - 1 points, each of which is connected
    to exactly one point in one of the other cliques; thus, the points form
    a perfect matching on k(k - 1) vertices.

--- pointedBlockChain.m
+++ pointedBlockChain.m
@@ -1,10 +1,48 @@
 % Generate the transition matrix of a random walk on the graph G obtained as follows:
 % Start with k disjoint s-cliques
-% Designate a vertex in each clique
-% Connect all the designated vertices, forming a k-clique
-function P = pointedBlockChain(s, k)
+%
+% If type == 1, each clique has a single "point" connected to all other points;
+% thus, the points form a k-clique.
+%
+% If type == 2, each clique has k - 1 points, each of which is connected to exactly
+% one point in another clique, with exactly one edge between points in any pair of cliques;
+% thus, the points form a perfect matching on k(k - 1) vertices.
+%
+function P = pointedBlockChain(s, k, type)
   n = s * k;
-  E = zeros(s);
-  E(1, 1) = 1;
-  P = normalize(kron(ones(k), E) + kron(eye(k), ones(s) - E));
+
+  if type == 1
+    E = zeros(s);
+    E(1, 1) = 1;
+    P = normalize(kron(ones(k), E) + kron(eye(k), ones(s) - E));
+
+  elseif type == 2
+    P = kron(eye(k), ones(s));
+
+    for i = 1 : k
+      minu = s * (i - 1) + 1;
+      for j = i + 1 : k
+
+	minv = s * (j - 1) + 1;
+	u = j - 1;
+	v = i;
+
+	u = minu + u - 1;
+	v = minv + v - 1;
+
+	P(u, v) = 1;
+	P(v, u) = 1;
+
+      end
+
+    end
+
+    P = normalize(P);
+
+  else
+
+    error('Error: invalid type (must be 1 or 2)', type);
+
+  end
+  
 end
