f2778801225fed9e225e4672600fde3dbc8ba26b
Sam Cole <smpcole@gmail.com>
2019-07-20 10:21:01 -0500

    Add function to test Kemeny's constant for Dirichelt random matrices

---
+++ testDirichKemeny.m
@@ -0,0 +1,23 @@
+function Y = testDirichKemeny(nmin, nmax, nstep, T)
+  x = nmin : nstep : nmax;
+  N = length(x);
+  Y = zeros(2, N);
+  Y(1, :) = x;
+  j = 1;
+  for n = x
+    D = dirichlet(ones(1, n));
+    A = zeros(n, n);
+    avgK = 0;
+    for t = 1 : T
+      for i = 1 : n
+	A(i, :) = D()';
+      end
+      A = normalize(A);
+      avgK = avgK + kemeny1(A);
+    end
+    avgK = avgK / T;
+    Y(2, j) = avgK;
+    j = j + 1;
+  end
+  
+end
