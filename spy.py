d057aaca2e435b1d48deca2cfbf34def2aaa2b4d
Sam Cole <smpcole@gmail.com>
2020-02-26 19:43:05 -0600

    Reduce size of markers in spy.py

--- spy.py
+++ spy.py
@@ -11,7 +11,7 @@ A = np.genfromtxt(csvpath, delimiter = ',')
 
 x, y = np.where(A) # Returns indices where A[i, j] != 0
 
-plt.scatter(x, y, marker = '.', color = 'blue')
+plt.scatter(x, y, marker = '.', color = 'blue', s = 1)
 
 plt.savefig(outpath)
 print("Output written to %s" % outpath)
