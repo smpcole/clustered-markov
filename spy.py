ab9a0067c7f094a7e314190776b1c6055538ec55
Sam Cole <samuel.cole@umanitoba.ca>
2020-02-28 17:22:53 -0600

    1st attempt at shading points based on value

--- spy.py
+++ spy.py
@@ -9,9 +9,18 @@ outpath = sys.argv[2]
 
 A = np.genfromtxt(csvpath, delimiter = ',')
 
+maxval = np.max(A)
+
 x, y = np.where(A) # Returns indices where A[i, j] != 0
 
-plt.scatter(x, y, marker = '.', color = 'blue', s = 1)
+numpts = x.size
+colors = np.zeros([numpts, 3])
+
+
+for i in range(numpts):
+    colors[i, 2] = 1 - A[x[i], y[i]] / maxval
+
+plt.scatter(x, y, marker = '.', c = colors, s = 72, edgecolors = 'face')
 
 plt.savefig(outpath)
 print("Output written to %s" % outpath)
