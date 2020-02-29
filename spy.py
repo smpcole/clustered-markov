ef857b626ee79370a04443eaf84d949bc95099c7
Sam Cole <smpcole@gmail.com>
2020-02-28 21:15:03 -0600

    Finish implementing shading

--- spy.py
+++ spy.py
@@ -14,13 +14,13 @@ maxval = np.max(A)
 x, y = np.where(A) # Returns indices where A[i, j] != 0
 
 numpts = x.size
-colors = np.zeros([numpts, 3])
+colors = np.ones([numpts, 3])
 
 
 for i in range(numpts):
-    colors[i, 2] = 1 - A[x[i], y[i]] / maxval
+    colors[i, 0 : 2] = 1 - A[x[i], y[i]] / maxval
 
-plt.scatter(x, y, marker = '.', c = colors, s = 72, edgecolors = 'face')
+plt.scatter(x, y, marker = '.', c = colors, s = 1, edgecolors = 'face')
 
 plt.savefig(outpath)
 print("Output written to %s" % outpath)
