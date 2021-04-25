6c7701d29ba2181f69186b180fd63cc358c1fc3e
Sam Cole <smpcole@gmail.com>
2021-04-25 18:30:02 -0500

    Return min indices of all blocks in EACH iteration of svdAlg

--- svdAlg.m
+++ svdAlg.m
@@ -4,7 +4,7 @@ function [Tperms, perms, minindices] = svdAlg(T, tol)
   
   Tperms = {};
 
-  [perms, minindices] = svdAlgRec(T, 1, tol, 1 : size(T, 1));
+  [perms, minindices] = svdAlgRec(T, 1, tol, 1 : size(T, 1), {1});
 
   numiter = size(perms, 1);
   Tperms{1} = T;
@@ -14,7 +14,7 @@ function [Tperms, perms, minindices] = svdAlg(T, tol)
   
 end
 
-function [perms, minindices] = svdAlgRec(T, minindex, tol, perms)
+function [perms, minindices] = svdAlgRec(T, minindex, tol, perms, minindices)
 
   T = dnf(T);
 
@@ -23,8 +23,6 @@ function [perms, minindices] = svdAlgRec(T, minindex, tol, perms)
   [U, S, V] = svd(L);
 
   n = size(T, 1);
-
-  minindices = minindex;
 
   if n > 1 && S(n - 1, n - 1) < tol
 
@@ -47,11 +45,19 @@ function [perms, minindices] = svdAlgRec(T, minindex, tol, perms)
     T1 = T(I, I);
     T2 = T(J, J);
 
-    [perms, minindices] = svdAlgRec(T1, minindex, tol, perms);
-    [perms, minindices2] = svdAlgRec(T2, minindex + size(T1, 1), tol, perms);
-    minindices = [minindices, minindices2];
+    minindices{end + 1} = merge(minindices{end}, [minindex, minindex + size(T1, 1)]);
+
+    [perms, minindices] = svdAlgRec(T1, minindex, tol, perms, minindices);
+    [perms, minindices] = svdAlgRec(T2, minindex + size(T1, 1), tol, perms, minindices);
     
   end
 
+end
+
+function z = merge(x, y) % Merge two sorted vectors; assume length(y) == 2
+  pre = find(x < y(1));
+  mid = find(and(y(1) < x, x < y(2)));
+  post = find(x > y(2));
+  z = [x(pre), y(1), x(mid), y(2), x(post)];
 end
 
